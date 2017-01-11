e61e362a05f752732124aa37dc2f7bcaa457e6b1
mindziukasss <mindziukasss@gmail.com>
2017-01-11 17:30:03 +0200

    Set global styles all application componenets

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
 	moduleId: module.id,
 	selector: 'app',
 	templateUrl: 'app.component.html' 
+	styleUrls: ['app.component.css' ]
+
 })
 export class AppComponent {
 	title: 'Freelance Bootcamp Dashboard';
--- app/documents/documents.component.js
+++ app/documents/documents.component.js
@@ -18,21 +18,21 @@ var DocumentsComponent = (function () {
                 description: 'addsfdfsd fsgf',
                 file_url: 'http://google.com',
                 updated_at: '01/09/17',
-                image_url: 'http://google.com',
+                image_url: 'http://www.top-gadgets.net/gallery/things-you-should-know-before-hiring-a-freelancer-picture/Things-you-should-know-before-hiring-a-freelancer.jpg',
             },
             {
                 title: "My second Doc",
                 description: 'addsfdfsd fsgf',
                 file_url: 'http://google.com',
                 updated_at: '01/09/17',
-                image_url: 'http://google.com',
+                image_url: 'https://udemy-images.udemy.com/course/750x422/54155_ffa4_6.jpg',
             },
             {
                 title: "My Last Doc",
                 description: 'addsfdfsd fsgf',
                 file_url: 'http://google.com',
                 updated_at: '01/09/17',
-                image_url: 'http://google.com',
+                image_url: 'https://www.policygenius.com/blog/library/uploads/2015/07/Freelancer-Long-Term-Disability-Featured.png',
             }
         ];
     }
@@ -42,7 +42,8 @@ DocumentsComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
         selector: 'documents',
-        templateUrl: 'documents.component.html'
+        templateUrl: 'documents.component.html',
+        styleUrls: ['documents.component.css']
     }),
     __metadata("design:paramtypes", [])
 ], DocumentsComponent);
